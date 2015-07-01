02cbd9b786611ff4c5c31ba7e1842f972d3bd2e6
icelxt <icelxt@163.com>
2015-07-01 19:32:41 +0800

    更新test实体

---
+++ src/main/java/wl/action/TestAction.java
@@ -0,0 +1,16 @@
+package wl.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TestAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+}
