39c2c6fd59badd4ff7d29dd161bff5541164949d
icelxt <icelxt@163.com>
2015-07-02 08:24:17 +0800

    配置hibernate

---
+++ src/main/java/wl/entity/Test.java
@@ -0,0 +1,24 @@
+package wl.entity;
+
+public class Test {
+
+	private String id;
+	private String loginName;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getLoginName() {
+		return loginName;
+	}
+
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+
+}
